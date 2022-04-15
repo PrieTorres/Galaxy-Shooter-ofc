b89381790fe9f6676c88d598a27e9df71166e549
Priscila <priscilatorres2023@gmail.com>
2022-04-14 22:05:29 -0300

    primeiro commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+** toda a parte que envolve a nave encostar no asteroid e explodir está comentada !!deu erro no eixo x estava considerando apenas o y
+** todo o sistema de níveis precisa ser revisado e melhorado
+** refazer o criador de asteroids para eles surgirem de maneira automática fora do eventListener e de acordo com os níveis
+** melhorar botão de pause (como nos testes repositório de testes)
+** fazer sistema de placar (do jeito que der, talvez envolva php)
---
+++ script.js
@@ -0,0 +1,175 @@
+let x = 0;
+let y = 0;
+let velTiro = 14.27848;
+let velAsteroid = 8;
+let xAster = 0;
+let tamanhopcX = Number(parseInt(window.screen.width)) - 40;
+let tamanhopcY = Number(parseInt(window.screen.availHeight)) - 120;
+let vida = 400;
+let pontos = 0;
+let nivel = 1;
+let perdeu = 0;
+//
+let bodye = document.body;
+let gameOverTela = document.querySelector('#game-over');
+//
+
+function pausar(){
+    window.alert('jogo pausado');
+}
+
+
+function reset(){
+    vida = 400;
+    pontos = 0;
+    velTiro = 14.27848;
+    velAsteroid = 8;
+    bodye.style.border = "none";
+    nivel = 1;
+    perdeu = 0;
+    document.body.querySelector('#pontos').innerHTML = pontos;
+}
+
+function criadorDeAsteroid(num, quantidade){
+    let i = quantidade;
+    bodye.innerHTML += '<div class="asteroid" style= "top:-40px; left:'+num+'px;"></div>';
+}
+
+function perderVida(){
+    let barraVida = document.body.querySelector('#barra-main');
+    vida -= 4;
+    barraVida.style.width = vida+'px';
+    bodye.style.border = "10px solid red"
+}
+/* o eixo x se altera conforme move a nave ***deu erro
+bodye.addEventListener('mousemove', m => {
+    x = m.pageX;
+    console.log(x);
+})*/
+
+let funcaoMain = e => {
+    x = e.pageX;
+    y = e.pageY;
+    xAster = Number(Math.floor(Math.random() * tamanhopcX));
+    criadorDeAsteroid(xAster);
+
+    if(vida <= 0){//-----------MORREU---------------
+        bodye.querySelector('#game-over').style.display = 'block'
+        perdeu = 1;
+    }
+
+    bodye.innerHTML += '<div class="tiro" style= "top: '+y+'px; left:'+x+'px;"></div>';
+    let dives = document.body.querySelectorAll('div.tiro');
+    let asteroides = document.body.querySelectorAll('div.asteroid');
+    let numCordX;
+    let numCordY;
+    dives.forEach(div => {
+        setInterval(function(){
+            numCordY = parseInt(div.style.top);
+            numCordX = parseInt(div.style.left);
+            if(numCordY >= -20){
+                numCordY -= velTiro;
+            }
+            div.style.top = numCordY+'px'; 
+        }, 20)
+        if(parseInt(div.style.top) <= -10){
+            if (div.parentNode) {
+                div.parentNode.removeChild(div);
+            }
+        }
+    });
+
+    asteroides.forEach(aster => {
+        let cordYaster;
+        
+        setInterval(function(){
+            cordYaster = parseInt(aster.style.top);
+            if(cordYaster < tamanhopcY){
+                cordYaster += velAsteroid;
+            }
+            aster.style.top = cordYaster+'px';
+
+            cordYaster = parseInt(aster.style.top);
+            if(cordYaster >= (tamanhopcY - 10)){
+                if (aster.parentNode) {
+                    aster.parentNode.removeChild(aster);
+                    perderVida();
+                }
+            }
+
+            let cordXaster = parseInt(aster.style.left);
+            cordYaster = parseInt(aster.style.top);
+            let maxX = cordXaster + 46;
+            let minX = cordXaster - 10;
+            let minY = cordYaster - 46;
+            let maxY = cordYaster + 36;
+            //conferidor se o tiro acertou
+            if((numCordX <= maxX && numCordX >= minX) && (numCordY <= maxY && numCordY >= minY) || (numCordX == cordXaster && numCordY == cordYaster)){
+                if (aster.parentNode) {
+                    aster.parentNode.removeChild(aster);
+                    pontos+= nivel;
+                    //niveis----------->
+                    if(pontos == 10){
+                        nivel = 2;
+                        window.alert('você passou de nível!');
+                        velAsteroid++;
+                    }
+                    if(pontos == 50){
+                        nivel = 3;
+                        window.alert('você passou de nível!');
+                        velAsteroid+=2;
+                    }
+                    if(pontos == 110){
+                        nivel = 3;
+                        window.alert('você passou de nível!');
+                        velAsteroid+=3;
+                        velTiro+=2;
+                    }
+                    if(pontos == 230){
+                        nivel = 4;
+                        window.alert('você passou de nível!');
+                        velAsteroid+=4;
+                        velTiro+=3;
+                    }
+                    if(pontos >= 999){
+                        document.body.querySelector('#pontos').style.fontSize = '40px' 
+                    }
+                    //<-----------------
+                    document.body.querySelector('#pontos').innerHTML = pontos;
+                    console.log(pontos);
+                    bodye.style.border = "none";
+                }
+            }
+            /* se a nave atingir a mesma coordenada x que um asteroid ao mesmo tempo que atinge os limites das coordenadas y do asteroid
+            // ou seja se um asteroid bate na nave, o eixo y não está muito preciso
+            // o eixo x não está funcionando
+            if(x = cordXaster && (y >= minY && y <= maxY)){
+                if (aster.parentNode) {
+                    aster.style.background = "url('XCVT.gif') no-repeat";
+                    vida -= 20;
+                    setInterval(function(){},500);
+                    aster.parentNode.removeChild(aster);
+                }
+            }*/
+        }, 50);
+
+    });
+    
+    if(perdeu == 1){
+        console.log('entrou no if')
+        bodye.addEventListener('keypress', function(){
+            bodye.removeEventListener('click', funcaoMain);
+            reset();
+            console.log('resetou');
+            gameOn();
+            bodye.style.border = 'none';
+        });
+    }
+}
+
+function gameOn(){
+    bodye.addEventListener('click', funcaoMain);
+}
+
+gameOn();
+
